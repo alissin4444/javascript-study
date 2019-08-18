e41f8ad6227f80ca8dcff3268af3a37a0ffd8822
Alisson Santos <45664013+alissin4444@users.noreply.github.com>
2019-08-18 12:28:13 -0300

    Update and rename letVSvar.js to varLetConst.js

---
+++ ES6/varLetConst.js
@@ -0,0 +1,8 @@
+// São tipos de variáveis
+
+
+// Var = functional scope
+//   Você pode declarar esta variável em qualquer lugar do escopo da função.
+// Let = Block scope
+//   Você precisa declarar ela no topo, mesmo sem atribuir valor, caso contrário surgirá um erro
+// Possúi valor constante
