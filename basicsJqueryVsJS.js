7035e584ac1ec05da37d7d53705581e83fae0029
Alisson Santos <45664013+alissin4444@users.noreply.github.com>
2019-10-09 11:48:23 -0300

    Create basicsJqueryVsJS.js

---
+++ basicsJqueryVsJS.js
@@ -0,0 +1,41 @@
+// Jquery ->
+  // $("div");  
+// JS -> 
+  // document.getElementsByTagName("div"); 
+// 
+
+// Jquery -> 
+  // 
+// JS -> 
+  // 
+  
+// Jquery -> 
+  // 
+// JS -> 
+  // 
+  
+// Jquery -> 
+  // 
+// JS -> 
+  // 
+  
+// Jquery -> 
+  // 
+// JS -> 
+  // 
+  
+// Jquery -> 
+  // 
+// JS -> 
+  // 
+  
+// Jquery -> 
+  // 
+// JS -> 
+  // 
+  
+// Jquery -> 
+  // 
+// JS -> 
+  // 
+  
